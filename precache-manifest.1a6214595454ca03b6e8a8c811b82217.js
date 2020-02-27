bf7d5a2e15bab8aabd6405bde52ca827db306ac9
agungsaputra <agung.saputra@weekendinc.com>
2020-02-27 10:40:52 +0700

    Menyalin isi folder build

---
+++ precache-manifest.1a6214595454ca03b6e8a8c811b82217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673455a30812619215a43917fc9a3e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bd1d19616021f69f50d",
+    "url": "/static/css/main.b377b961.chunk.css"
+  },
+  {
+    "revision": "6a643ab1d6af8fbf9dd6",
+    "url": "/static/js/2.d4ff246e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d4ff246e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd1d19616021f69f50d",
+    "url": "/static/js/main.9017bba5.chunk.js"
+  },
+  {
+    "revision": "e19d420bcf84f924e3fc",
+    "url": "/static/js/runtime-main.0efe0212.js"
+  }
+]);
